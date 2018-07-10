a13d33a39a040656f384b94593556b731cd1464b
LIM42SNEC <lim42snec@gmail.com>
2018-07-10 21:09:09 +0800

    [+]实现CRUD功能

--- WPF-控件ListView/MainWindow.xaml.cs
+++ WPF-控件ListView/MainWindow.xaml.cs
@@ -10,28 +10,68 @@ namespace 控件ListView
     /// </summary>
     public partial class MainWindow : Window
     {
+        public SQLiteConnection Conn { get; set; }
+
         public MainWindow()
         {
             InitializeComponent();
+            Conn = new SQLiteConnection("test.db");
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            var conn = new SQLiteConnection("test.db");
-            var Persons = conn.Table<Person>().ToList();
+            var Persons = Conn.Table<Person>().ToList();
             listView1.ItemsSource = Persons;
 
         }
-    }
 
-  
-    // 实体类名必须与数据库中表名一致
-    public class Person
-    {
-        [PrimaryKey, AutoIncrement]
-        public Int64 Id { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Email { get; set; }
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            //Conn.Execute("")
+            var firstName = txtFirstName.Text;
+            var lastName = txtLastName.Text;
+            var email = txtEmail.Text;
+
+            Conn.Insert(new Person()
+            {
+                FirstName = firstName,
+                LastName = lastName,
+                Email = email
+            });
+
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            var id = txtId.Text;
+            var firstName = txtFirstName.Text;
+            var lastName = txtLastName.Text;
+            var email = txtEmail.Text;
+
+            string sql = string.Format("UPDATE Person SET FirstName = '{0}', LastName='{1}', Email='{2}' WHERE ID = {3}",
+                firstName,
+                lastName,
+                email,
+                id); 
+            Conn.Execute(sql);
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            var id = txtId.Text;
+            string sql = string.Format("DELETE FROM Person WHERE ID = {0}", id);
+            Conn.Execute(sql);
+        }
+
+
+        // 实体类名必须与数据库中表名一致
+        public class Person
+        {
+            [PrimaryKey, AutoIncrement]
+            public Int64 Id { get; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public string Email { get; set; }
+        }
     }
 }
