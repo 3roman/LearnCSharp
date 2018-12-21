6ee803e39797493ba9f82a4db6968760b5235620
LIM42SNEC <lim42snec@gmail.com>
2018-12-21 23:05:01 +0800

    [+]add CSharp_Attribute project

---
+++ CSharp-Attribute/Person.cs
@@ -0,0 +1,12 @@
+﻿namespace CSharp_Attribute
+{
+    class Person
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public int Age { get; set; }
+        [Required]
+        public bool Married { get; set; }
+    }
+}
---
+++ CSharp-Attribute/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace CSharp_Attribute
+{
+    [Version(MajorVersion =1, MinorVersion = 0, Description ="测试版")]
+    class Program
+    {
+        //[Version(MajorVersion = 1, MinorVersion = 0, Description = "测试版")]
+        static void Main(string[] args)
+        {
+
+            var p1 = new Person()
+            {
+               Age = 22
+            };
+
+            if (RequiredAttribute.IsPropertyRequired(p1))
+            {
+                Console.WriteLine("全部已经赋值");
+            }
+            else
+            {
+                Console.WriteLine("存在未赋值");
+            }
+
+
+            var p2 = new Person()
+            {
+                Name = "Tom",
+                Age=28,
+                Married = true
+            };
+
+            if (RequiredAttribute.IsPropertyRequired(p2))
+            {
+                Console.WriteLine("全部已经赋值");
+            }
+            else
+            {
+                Console.WriteLine("存在未赋值");
+            }
+
+            var attribute = (VersionAttribute)Attribute.GetCustomAttribute(typeof(Program), typeof(VersionAttribute));
+
+            Console.WriteLine(attribute.Description);
+
+            Console.Read();
+        }
+    }
+}
