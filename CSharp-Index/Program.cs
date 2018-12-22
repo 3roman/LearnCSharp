6f070647c3d1c75064a93938f82453cddc386f67
LIM42SNEC <lim42snec@gmail.com>
2018-12-22 22:39:27 +0800

    [+]Add CSharp-Index

---
+++ CSharp-Index/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace CSharp_Index
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = new Student();
+            s[0] = "小明";
+            s[1] = "丽丽";
+
+            for (int i = 0; i < 2; i++)
+            {
+                System.Console.WriteLine(s[i]);
+            }
+
+            System.Console.Read();
+        }
+    }
+
+
+    class Student
+    {
+        private string[] names = new string[100];
+
+        // 声明索引器
+        public string this[int index]
+        {
+            get { return names[index]; }
+            set { names[index] = value; }
+        }
+    }
+}
