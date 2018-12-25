7bd68235dc0c03482b5e4dc92165205f2fb9a127
LIM42SNEC <lim42snec@gmail.com>
2018-12-25 22:35:10 +0800

    [*]modify csharp-arttribute

--- CSharp-Attribute/Program.cs
+++ CSharp-Attribute/Program.cs
@@ -1,50 +1,73 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Reflection;
 
 namespace CSharp_Attribute
 {
-    [Version(MajorVersion =1, MinorVersion = 0, Description ="测试版")]
     class Program
     {
         //[Version(MajorVersion = 1, MinorVersion = 0, Description = "测试版")]
         static void Main(string[] args)
         {
+            // 只有在DEBUG模式下才会调用该函数
+            MyClass.Foo();
+            MyClass.SayHello();
 
-            var p1 = new Person()
+            // 通过反射获取attribute信息
+            HelpAttribute ha;
+            var attributes = typeof(MyClass).GetCustomAttributes();
+            foreach (var attr in attributes)
             {
-               Age = 22
-            };
+                // 类型不兼容返回null
+                ha = attr as HelpAttribute;
+                if (null!=ha)
+                {
+                    Console.WriteLine(ha.HelpMessage);
+                    Console.WriteLine(ha.MajorVersion);
+                }
 
-            if (RequiredAttribute.IsPropertyRequired(p1))
-            {
-                Console.WriteLine("全部已经赋值");
             }
-            else
-            {
-                Console.WriteLine("存在未赋值");
-            }
-
-
-            var p2 = new Person()
-            {
-                Name = "Tom",
-                Age=28,
-                Married = true
-            };
-
-            if (RequiredAttribute.IsPropertyRequired(p2))
-            {
-                Console.WriteLine("全部已经赋值");
-            }
-            else
-            {
-                Console.WriteLine("存在未赋值");
-            }
-
-            var attribute = (VersionAttribute)Attribute.GetCustomAttribute(typeof(Program), typeof(VersionAttribute));
-
-            Console.WriteLine(attribute.Description);
 
             Console.Read();
+        }
+    }
+
+
+    [Help("This is a test class", MajorVersion =1)]
+    class MyClass
+    {
+        [Conditional("DEBUG")]
+        public static void Foo()
+        {
+            Console.WriteLine("In Debug Mode");
+        }
+
+        // 加true参数调用过时方法会报错
+        //[Obsolete("奥巴马下台了，不要再教他总统", true)]
+        [Obsolete("奥巴马下台了，不要再教他总统")]
+        public static void SayHello()
+        {
+            Console.WriteLine("hello 奥巴马总统");
+        }
+    }
+
+    // 该attribute只能用来修饰类，且只能一次修饰一个类,不会自动传递到派生类
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited =false)]
+    class HelpAttribute:Attribute
+    {
+        public int MajorVersion { get; set; }
+
+        private string _helpMessage;
+
+        public string HelpMessage
+        {
+            get { return _helpMessage; }
+            set { _helpMessage = value; }
+        }
+
+        public HelpAttribute(string para)
+        {
+            _helpMessage = para;  
         }
     }
 }
