befa2dadb0224b74ff7ed5d25c3cdd1530a11f61
LIM42SNEC <lim42snec@gmail.com>
2018-12-26 20:52:20 +0800

    [+]Add CSharp-Yield

---
+++ CSharp-Yield/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System;
+
+namespace CSharp_Yield
+{
+    class Program
+    {
+        static IEnumerable<int> GetNumbers(int lbound, int ubound, int maxCount)
+        {
+            int count = 0;
+            for (int i = lbound; i <= ubound; i++)
+            {
+                
+                if (count < maxCount)
+                {
+                    yield return i;
+                }
+                else
+                {
+                    // 跳出整个方法
+                    yield break;
+                }
+                count++;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var numbers = GetNumbers(1, 100, 10);
+            numbers.ToList().ForEach(x => Console.WriteLine(x));
+            Console.Read();
+        }
+
+    }
+
+}
