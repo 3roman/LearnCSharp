53c6ddd79dc005bc6998ea496cc9ef49eb25bfa6
LIM42SNEC <lim42snec@gmail.com>
2018-12-28 22:18:49 +0800

    [+]Add CSharp-Event

---
+++ CSharp-Event/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace CSharp_Event
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestEvent te = new TestEvent();
+            te.RaiseValueChanged(null, null);
+        }
+    }
+
+
+    class TestEvent
+    {
+        // 定义事件
+        public event EventHandler ValueChanged;
+
+        // 响应函数
+        private void OnValueChanged(object sender, EventArgs e)
+        {
+            Console.WriteLine("Value Changed");
+        }
+
+        // 事件绑定
+        public TestEvent()
+        {
+            ValueChanged += OnValueChanged;
+        }
+
+        // 触发事件
+        public void RaiseValueChanged(object sender, EventArgs e)
+        {
+            ValueChanged?.Invoke(sender, e);
+        }
+    }
+}
