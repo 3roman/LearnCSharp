dba67624372e65bc45419dbcf571c70287e808c2
LIM42SNEC <lim42snec@gmail.com>
2019-01-05 16:31:56 +0800

    [+]Add Nuget-Dapper

---
+++ Nuget-Dapper/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using Dapper;
+using System.Data.SQLite;
+
+namespace Nuget_Dapper
+{
+    class Program
+    {
+        const string ConnectionString = "Data Source=school.db";
+
+
+        static void Main(string[] args)
+        {
+            using (var conn = new SQLiteConnection(ConnectionString))
+            {
+                var teachers = conn.Query<Teacher>("SELECT * FROM tbl_teacher");
+                teachers.ToList().ForEach(x =>
+                {
+                    Console.WriteLine(string.Format("{0}-{1}-{2}-{3}",
+                        x.ID, 
+                        x.Name, 
+                        x.Age,
+                        x.Gender ? "男":"女"));
+                });
+
+                Console.Read();
+            }
+        }
+    }
+}
