7a5f996f36312e9d7cd76e5abf82759746a110f4
LIM42SNEC <lim42snec@gmail.com>
2019-04-22 22:30:43 +0800

    [+]add MVVM-MVVM proj
    NET>=4.5

---
+++ MVVM-SimpleMVVMNuget/ViewModel/MainViewModel.cs
@@ -0,0 +1,36 @@
+﻿using Mvvm;
+using System.Windows.Input;
+using System.Windows;
+using Mvvm.Commands;
+
+namespace WpfApp1.ViewModel
+{
+    class MainViewModel:BindableBase
+    {
+        public ICommand ShowBoxCommand { get; }
+        public string Message { get; set; }
+
+        public MainViewModel()
+        {
+            Message = "great mvvm pattern";
+            ShowBoxCommand = new DelegateCommand(ShowBox, CanShowBox);
+        }
+
+        private void ShowBox()
+        {
+            MessageBox.Show(Message);
+        }
+
+        private bool CanShowBox()
+        {
+            if (string.IsNullOrEmpty(Message))
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+
+    }
+}
